97dbec05a32b6dfd86e723ca83623c8405f8c6b1
thegecko <rob.moran@arm.com>
2021-08-30 18:44:54 +0100

    use USB in main process only

--- main.js
+++ main.js
@@ -1,42 +1,61 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const usb = require('usb');
+
+let windows = []
+
+const showDevices = () => {
+    const devices = usb.getDeviceList()
+    const text = devices.map(d => `pid: ${d.deviceDescriptor.idProduct}, vid: ${d.deviceDescriptor.idVendor}`).join('\n')
+    windows.forEach(win => {
+        if (win) win.webContents.send('devices', text)
+    })
+}
 
 function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // and load the index.html of the app.
+    win.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+
+    windows.push(win);
+    showDevices()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+    usb.on('attach', showDevices)
+    usb.on('detach', showDevices)
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    createWindow()
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    usb.removeListener('attach', showDevices)
+    usb.removeListener('detach', showDevices)
+    app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-const usb = require('usb');
+const { ipcRenderer } = require('electron')
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
@@ -12,13 +12,5 @@ window.addEventListener('DOMContentLoaded', () => {
         replaceText(`${dependency}-version`, process.versions[dependency])
     }
 
-    const showDevices = () => {
-        const devices = usb.getDeviceList()
-        const data = devices.map(d => `pid: ${d.deviceDescriptor.idProduct}, vid: ${d.deviceDescriptor.idVendor}`).join('\n')
-        replaceText('devices', data)
-    }
-
-    usb.on('attach', showDevices)
-    usb.on('detach', showDevices)
-    showDevices()
+    ipcRenderer.on('devices', (_event, text) => replaceText('devices', text))
 })
